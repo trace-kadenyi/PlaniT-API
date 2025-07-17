48fee4f138b808d7454cf5b7313871e854a007fc
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 15:59:39 +0300

    use root route

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
+// port
 const PORT = process.env.PORT || 4000;
 
+// path
 const path = require("path");
+
+// import routes
+const root = require("./routes/root")
 
 require('dotenv').config();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+// use routes
+app.use("/", root)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
