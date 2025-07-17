6c8e0a840433b7160bbc566dd345d9c51f3be809
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 20:36:19 +0300

    Remove error when no tasks are found

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -16,9 +16,6 @@ const getAllEvents = async (req, res) => {
   try {
     const events = await Event.find().sort({ createdAt: -1 });
 
-    if (events.length === 0) {
-      return res.status(404).json({ message: "No events." });
-    }
     res.json(events);
   } catch (err) {
     res.status(500).json({ message: err.message });
