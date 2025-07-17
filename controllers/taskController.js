1481d7983508017502dd77123c7aa6cf1c589d96
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 18:33:49 +0300

    create task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -14,7 +14,16 @@ const getAllTasks = async (req, res) => {
   }
 };
 
-
+// Create a new task
+const createTask = async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
 
 
