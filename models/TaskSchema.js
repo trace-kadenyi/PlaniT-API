85169684c279ed549309f2b6bc5e6f9334b05cd1
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 18:34:35 +0300

    task schema

---
+++ models/TaskSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    assignedTo: {
+      type: String, // you can change this to a User ObjectId if you add user support
+    },
+    deadline: Date,
+    priority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    status: {
+      type: String,
+      enum: ["To Do", "In Progress", "Completed"],
+      default: "To Do",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
