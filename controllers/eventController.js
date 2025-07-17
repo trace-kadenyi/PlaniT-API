3152ebbe886bfd7baa434389cf523534125c0573
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-17 18:45:59 +0300

    create event op

---
+++ controllers/eventController.js
@@ -0,0 +1,15 @@
+
+const Event = require("../models/EventSchema");
+
+
+const createEvent = async (req, res) => {
+  try {
+    const event = new Event(req.body);
+    const savedEvent = await event.save();
+    res.status(201).json(savedEvent);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+
