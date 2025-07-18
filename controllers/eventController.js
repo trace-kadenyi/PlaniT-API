ff8b2705cab1177e910f8e6a146c23c0b133d025
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-18 14:07:58 +0300

    Add CRUD ops to handle events

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,6 +1,6 @@
 const Event = require("../models/EventSchema");
 
-// create event
+// Create a new event
 const createEvent = async (req, res) => {
   try {
     const event = new Event(req.body);
@@ -11,12 +11,54 @@ const createEvent = async (req, res) => {
   }
 };
 
-// get all events
+// Get all events
 const getAllEvents = async (req, res) => {
   try {
     const events = await Event.find().sort({ createdAt: -1 });
-
     res.json(events);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Get event by ID
+const getEventById = async (req, res) => {
+  try {
+    const event = await Event.findById(req.params.id);
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+    res.json(event);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Update an event
+const updateEvent = async (req, res) => {
+  try {
+    const updatedEvent = await Event.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true } // return the updated doc
+    );
+    if (!updatedEvent) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+    res.json(updatedEvent);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Delete an event
+const deleteEvent = async (req, res) => {
+  try {
+    const deletedEvent = await Event.findByIdAndDelete(req.params.id);
+    if (!deletedEvent) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+    res.json({ message: "Event deleted" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -25,4 +67,7 @@ const getAllEvents = async (req, res) => {
 module.exports = {
   createEvent,
   getAllEvents,
+  getEventById,
+  updateEvent,
+  deleteEvent,
 };
