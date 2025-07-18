17c7bc56b4f8b3c04d3a385131c57d9bf536be07
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-18 15:12:37 +0300

    delete tasks associated with deleted event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -55,10 +55,15 @@ const updateEvent = async (req, res) => {
 const deleteEvent = async (req, res) => {
   try {
     const deletedEvent = await Event.findByIdAndDelete(req.params.id);
+
     if (!deletedEvent) {
       return res.status(404).json({ message: "Event not found" });
     }
-    res.json({ message: "Event deleted" });
+
+    // Cascade delete: remove tasks tied to this event
+    await Task.deleteMany({ eventId: req.params.id });
+
+    res.json({ message: "Event and associated tasks deleted" });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
