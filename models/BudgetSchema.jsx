a702a46d5df7bf4f4e7a235bb63c15a1268f4d11
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-24 17:20:31 +0300

    Add budgetschema

---
+++ models/BudgetSchema.jsx
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const budgetSchema = new mongoose.Schema(
+  {
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: [true, "Budget must be linked to an event"],
+      unique: true,
+    },
+    totalBudget: {
+      type: Number,
+      required: true,
+      min: [0, "Budget must be 0 or more"],
+    },
+    notes: {
+      type: String,
+      trim: true,
+      maxlength: 300,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+budgetSchema.index({ eventId: 1 });
+
+module.exports = mongoose.model("Budget", budgetSchema);
