7addaf73bf4d67af939c54a0994f55e895657579
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-25 14:26:43 +0300

    modify routes

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -8,11 +8,11 @@ const {  createExpense,
   getExpensesSummary} = require("../controllers/expenseController")
 
 
-  router.post('/expenses', createExpense);
-router.get('/events/:eventId/expenses', getExpensesByEventId);
-router.get('/expenses/:id', getExpenseById);
-router.put('/expenses/:id', updateExpense);
-router.delete('/expenses/:id', deleteExpense);
-router.get('/events/:eventId/expenses/summary', getExpensesSummary);
+  router.post('/', createExpense);
+router.get('/:eventId/expenses', getExpensesByEventId);
+router.get('/:id', getExpenseById);
+router.put('/:id', updateExpense);
+router.delete('/:id', deleteExpense);
+router.get('/:eventId/expenses/summary', getExpensesSummary);
 
 module.exports = router;
