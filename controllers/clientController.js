562d1bcf343ade1a370383a4259c773e32f51449
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-31 19:09:12 +0300

    update client op

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -36,8 +36,30 @@ const getClientWithEvents = async (req, res) => {
   }
 };
 
+// Update a client
+const updateClient = async (req, res) => {
+  try {
+    const client = await Client.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true, runValidators: true }
+    );
+
+    if (!client) {
+      return res.status(404).json({ error: "Client not found" });
+    }
+
+    res.json(client);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+
+
 module.exports = {
   createClient,
   getAllClients,
   getClientWithEvents,
+  
 };
