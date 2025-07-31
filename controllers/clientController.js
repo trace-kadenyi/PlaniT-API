9f91dcb129412906e5ab61070ab9eb4e5fcb9c34
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-31 18:06:05 +0300

    export all ops

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -35,3 +35,9 @@ const getClientWithEvents = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+module.exports = {
+  createClient,
+  getAllClients,
+  getClientWithEvents,
+};
