53975b315d22dc89028264910a90a0b935837390
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-13 12:58:40 +0300

    Restrict to certain roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -259,4 +259,15 @@ exports.protect = async (req, res, next) => {
   }
 };
 
-
+// Restrict to certain roles
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        status: "error",
+        message: "You do not have permission to perform this action",
+      });
+    }
+    next();
+  };
+};
