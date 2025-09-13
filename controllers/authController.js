e0e422ddee532cf27b14793ec6b3c65989367f93
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-13 12:58:27 +0300

    Protect middleware (to be used in routes)

--- controllers/authController.js
+++ controllers/authController.js
@@ -209,5 +209,54 @@ exports.resetPassword = async (req, res) => {
   }
 };
 
+// Protect middleware (to be used in routes)
+exports.protect = async (req, res, next) => {
+  try {
+    // 1) Getting token and check if it's there
+    let token;
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        status: "error",
+        message: "You are not logged in! Please log in to get access",
+      });
+    }
+
+    // 2) Verification token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return res.status(401).json({
+        status: "error",
+        message: "The user belonging to this token does no longer exist",
+      });
+    }
+
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return res.status(401).json({
+        status: "error",
+        message: "User recently changed password! Please log in again",
+      });
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    next();
+  } catch (err) {
+    res.status(401).json({
+      status: "error",
+      message: "Invalid token",
+    });
+  }
+};
 
 
