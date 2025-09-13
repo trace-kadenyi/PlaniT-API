51bf8156caedefef4ac961f4b7b21e0f1f8e7122
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-13 13:43:59 +0300

    add comments

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+// signup
 router.post("/signup", authController.signup);
+// login
 router.post("/login", authController.login);
+// refresh token
 router.post("/refresh-token", authController.refreshToken);
+// forgot password
 router.post("/forgot-password", authController.forgotPassword);
+// reset password
 router.patch("/reset-password/:token", authController.resetPassword);
 
 module.exports = router;
