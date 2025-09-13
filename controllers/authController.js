fba34142e6d42310584292fec329dd7c45d373bd
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-13 12:58:08 +0300

    reset password func

--- controllers/authController.js
+++ controllers/authController.js
@@ -171,6 +171,43 @@ exports.forgotPassword = async (req, res) => {
   }
 };
 
+// Reset password
+exports.resetPassword = async (req, res) => {
+  try {
+    // 1) Get user based on the token
+    const hashedToken = crypto
+      .createHash("sha256")
+      .update(req.params.token)
+      .digest("hex");
+
+    const user = await User.findOne({
+      passwordResetToken: hashedToken,
+      passwordResetExpires: { $gt: Date.now() },
+    });
+
+    // 2) If token has not expired, and there is user, set the new password
+    if (!user) {
+      return res.status(400).json({
+        status: "error",
+        message: "Token is invalid or has expired",
+      });
+    }
+
+    user.password = req.body.password;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    // 3) Update changedPasswordAt property for the user
+    // 4) Log the user in, send JWT
+    createSendToken(user, 200, res);
+  } catch (err) {
+    res.status(400).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+};
 
 
 
