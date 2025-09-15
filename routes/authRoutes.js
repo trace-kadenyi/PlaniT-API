8c8b63bd3523f5600827949752322a439cba5116
Tracey Kadenyi <treykadenyi@gmail.com>
2025-09-15 11:47:07 +0300

    modify file format

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,12 +4,16 @@ const authController = require("../controllers/authController");
 
 // signup
 router.post("/signup", authController.signup);
+
 // login
 router.post("/login", authController.login);
+
 // refresh token
 router.post("/refresh-token", authController.refreshToken);
+
 // forgot password
 router.post("/forgot-password", authController.forgotPassword);
+
 // reset password
 router.patch("/reset-password/:token", authController.resetPassword);
 
