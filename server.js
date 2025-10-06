6c1b71576432f411b7457938980cb8254b9f916d
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-06 16:38:49 +0300

    improve cors

--- server.js
+++ server.js
@@ -54,14 +54,18 @@ require("dotenv").config();
 app.use(express.json());
 
 // cors
+// cors - UPDATED CONFIG
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
+
+// Handle preflight requests explicitly
+// app.options('*', cors());
 
 // cookie-parser
 app.use(cookieParser());
