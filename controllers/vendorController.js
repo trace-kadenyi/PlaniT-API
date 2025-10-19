66c63745514f10cd82179151e282ad4aecc15480
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-19 20:06:17 +0300

    update getVendorStats func: Get all users in the same organization

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -154,7 +154,19 @@ const toggleVendorArchive = async (req, res) => {
 // Get vendor statistics
 const getVendorStats = async (req, res) => {
   try {
+    // Get all users in the same organization
+    const organizationUsers = await User.find({
+      organization: req.user.organization,
+    }).select("_id");
+
+    const organizationUserIds = organizationUsers.map((user) => user._id);
+
     const stats = await Vendor.aggregate([
+      {
+        $match: {
+          createdBy: { $in: organizationUserIds },
+        },
+      },
       {
         $group: {
           _id: "$services",
