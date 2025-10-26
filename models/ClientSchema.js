17e217ee5d408d01f128e7f1c72e101fa7297a5c
Tracey Kadenyi <treykadenyi@gmail.com>
2025-10-26 14:28:43 +0300

    add is deleted

--- models/ClientSchema.js
+++ models/ClientSchema.js
@@ -26,6 +26,12 @@ const clientSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    isDeleted: {
+      // soft deletion flag
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
     archivedAt: Date,
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
@@ -36,5 +42,20 @@ const clientSchema = new mongoose.Schema(
 
   { timestamps: true }
 );
+
+// Add middleware to handle population of deleted clients
+clientSchema.post("find", function (docs) {
+  docs.forEach((doc) => {
+    if (doc.isDeleted) {
+      doc.name = `${doc.name} (Deleted)`;
+    }
+  });
+});
+
+clientSchema.post("findOne", function (doc) {
+  if (doc && doc.isDeleted) {
+    doc.name = `${doc.name} (Deleted)`;
+  }
+});
 
 module.exports = mongoose.model("Client", clientSchema);
