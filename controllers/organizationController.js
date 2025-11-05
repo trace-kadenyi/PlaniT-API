03ae26a162fe26b03d2373da33c0b6eed8b7f5ce
Tracey Kadenyi <treykadenyi@gmail.com>
2025-11-05 17:18:08 +0300

    add getOrganizationDetails func

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -194,9 +194,32 @@ const updateUserRole = async (req, res) => {
   }
 };
 
+// Get organization details
+const getOrganizationDetails = async (req, res) => {
+  try {
+    const organization = await Organization.findById(req.user.organization);
+
+    if (!organization) {
+      return res.status(404).json({
+        message: "Organization not found",
+      });
+    }
+
+    res.json({
+      id: organization._id,
+      name: organization.name,
+      plan: organization.plan,
+      settings: organization.settings,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   addUserToOrganization,
   getOrganizationUsers,
   removeUserFromOrganization,
   updateUserRole,
+  getOrganizationDetails,
 };
