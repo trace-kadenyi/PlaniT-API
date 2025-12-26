a9052ede5bc759feddb4c6394842dfaadbbc9960
Tracey Kadenyi <treykadenyi@gmail.com>
2025-12-26 14:38:54 +0300

    user Routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,35 @@
+// routes/userRoutes.js
+const express = require("express");
+const router = express.Router();
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  updateUserRole,
+  deleteUser,
+} = require("../controllers/usersController");
+const { protect } = require("../controllers/authController");
+
+// All routes require authentication
+router.use(protect);
+
+// Get all users in organization
+router.get("/", getUsers);
+
+// Get single user
+router.get("/:userId", getUser);
+
+// Add new user
+router.post("/", createUser);
+
+// Update user details
+router.patch("/:userId", updateUser);
+
+// Update user role
+router.patch("/:userId/role", updateUserRole);
+
+// Delete user
+router.delete("/:userId", deleteUser);
+
+module.exports = router;
