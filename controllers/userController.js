a9cbfbd379fef3d4839a2f56e8d8378129ccbc09
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-06 14:15:40 +0300

    update user and import regex

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,8 +46,9 @@ const createUser = async (req, res) => {
 
     // Validate password
     if (password) {
+      // In userController.js
       const passwordRegex =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~])[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~]/;
       if (!passwordRegex.test(password)) {
         return res.status(400).json({
           message:
@@ -80,7 +81,9 @@ const createUser = async (req, res) => {
 
     await newUser.save();
 
-    const userResponse = await User.findById(newUser._id).select("-password -passwordResetToken -passwordResetExpires");
+    const userResponse = await User.findById(newUser._id).select(
+      "-password -passwordResetToken -passwordResetExpires"
+    );
 
     res.status(201).json({
       message: "User added successfully",
@@ -100,7 +103,7 @@ const createUser = async (req, res) => {
 // const updateUser = async (req, res) => {
 //   try {
 //     const { firstName, lastName, email, phone } = req.body;
-    
+
 //     // Find the target user
 //     const targetUser = await User.findOne({
 //       _id: req.params.userId,
@@ -141,22 +144,79 @@ const createUser = async (req, res) => {
 // };
 
 // Update user details - allow self-updates
+// const updateUser = async (req, res) => {
+//   try {
+//     const { firstName, lastName, email, phone } = req.body;
+
+//     // Find the target user
+//     const targetUser = await User.findOne({
+//       _id: req.params.userId,
+//       organization: req.user.organization,
+//     });
+
+//     if (!targetUser) {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+
+//     const isSelf = req.user._id.toString() === req.params.userId;
+
+//     // If editing someone else, check permissions
+//     if (!isSelf) {
+//       // Admins can't edit super admins
+//       if (req.user.role === "admin" && targetUser.role === "super_admin") {
+//         return res.status(403).json({
+//           message: "Cannot edit super admins",
+//         });
+//       }
+
+//       // Check if user has permission to edit others
+//       if (!["super_admin", "admin"].includes(req.user.role)) {
+//         return res.status(403).json({
+//           message: "Only admins can edit other users",
+//         });
+//       }
+//     }
+
+//     // Update fields - self can update basic info
+//     if (firstName) targetUser.firstName = firstName;
+//     if (lastName) targetUser.lastName = lastName;
+//     if (email) targetUser.email = email.toLowerCase();
+//     if (phone) targetUser.contact = { ...targetUser.contact, phone };
+
+//     await targetUser.save();
+
+//     const updatedUser = await User.findById(targetUser._id).select("-password -passwordResetToken -passwordResetExpires");
+
+//     res.json({
+//       message: "User updated successfully",
+//       user: updatedUser,
+//     });
+//   } catch (err) {
+//     if (err.code === 11000) {
+//       return res.status(400).json({ message: "Email already exists" });
+//     }
+//     res.status(500).json({ message: err.message });
+//   }
+// };
+
+// Update user details - allow password update
 const updateUser = async (req, res) => {
   try {
-    const { firstName, lastName, email, phone } = req.body;
-    
+    const { firstName, lastName, email, phone, newPassword, currentPassword } =
+      req.body;
+
     // Find the target user
     const targetUser = await User.findOne({
       _id: req.params.userId,
       organization: req.user.organization,
-    });
+    }).select("+password"); // Need to select password to verify current one
 
     if (!targetUser) {
       return res.status(404).json({ message: "User not found" });
     }
 
     const isSelf = req.user._id.toString() === req.params.userId;
-    
+
     // If editing someone else, check permissions
     if (!isSelf) {
       // Admins can't edit super admins
@@ -165,7 +225,7 @@ const updateUser = async (req, res) => {
           message: "Cannot edit super admins",
         });
       }
-      
+
       // Check if user has permission to edit others
       if (!["super_admin", "admin"].includes(req.user.role)) {
         return res.status(403).json({
@@ -174,15 +234,63 @@ const updateUser = async (req, res) => {
       }
     }
 
-    // Update fields - self can update basic info
+    // Update basic fields
     if (firstName) targetUser.firstName = firstName;
     if (lastName) targetUser.lastName = lastName;
     if (email) targetUser.email = email.toLowerCase();
     if (phone) targetUser.contact = { ...targetUser.contact, phone };
 
+    // Handle password change (only for self or admins with permission)
+    if (newPassword) {
+      // If changing own password, verify current password
+      if (isSelf) {
+        if (!currentPassword) {
+          return res.status(400).json({
+            message: "Current password is required to set a new password",
+          });
+        }
+
+        // Verify current password
+        const isPasswordCorrect = await targetUser.correctPassword(
+          currentPassword,
+          targetUser.password
+        );
+
+        if (!isPasswordCorrect) {
+          return res.status(401).json({
+            message: "Current password is incorrect",
+          });
+        }
+      }
+
+      // If admin changing someone else's password, no current password needed
+      // but they must have permission
+      else if (!["super_admin", "admin"].includes(req.user.role)) {
+        return res.status(403).json({
+          message: "Only admins can change other users' passwords",
+        });
+      }
+
+      // Validate new password
+      // In userController.js
+      const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~])[A-Za-z\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?`~]/;
+      if (!passwordRegex.test(newPassword)) {
+        return res.status(400).json({
+          message:
+            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+        });
+      }
+
+      // Set new password
+      targetUser.password = newPassword;
+    }
+
     await targetUser.save();
 
-    const updatedUser = await User.findById(targetUser._id).select("-password -passwordResetToken -passwordResetExpires");
+    const updatedUser = await User.findById(targetUser._id).select(
+      "-password -passwordResetToken -passwordResetExpires"
+    );
 
     res.json({
       message: "User updated successfully",
@@ -195,6 +303,7 @@ const updateUser = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
 // Update user role
 const updateUserRole = async (req, res) => {
   try {
@@ -226,7 +335,9 @@ const updateUserRole = async (req, res) => {
     targetUser.role = role;
     await targetUser.save();
 
-    const updatedUser = await User.findById(targetUser._id).select("-password -passwordResetToken -passwordResetExpires");
+    const updatedUser = await User.findById(targetUser._id).select(
+      "-password -passwordResetToken -passwordResetExpires"
+    );
 
     res.json({
       message: "User role updated successfully",
