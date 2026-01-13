ca0be1f1125133d2ad56103156b2f4de5417204c
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-14 00:38:27 +0300

    restore original code

--- models/ClientSchema.js
+++ models/ClientSchema.js
@@ -43,4 +43,19 @@ const clientSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Add middleware to handle population of deleted clients
+// clientSchema.post("find", function (docs) {
+//   docs.forEach((doc) => {
+//     if (doc.isDeleted) {
+//       doc.name = `${doc.name} (Deleted)`;
+//     }
+//   });
+// });
+
+// clientSchema.post("findOne", function (doc) {
+//   if (doc && doc.isDeleted) {
+//     doc.name = `${doc.name} (Deleted)`;
+//   }
+// });
+
 module.exports = mongoose.model("Client", clientSchema);
