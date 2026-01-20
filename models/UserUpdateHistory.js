c81b8af804f906be62648aac7cb301e62b28211a
Tracey Kadenyi <treykadenyi@gmail.com>
2026-01-20 18:23:51 +0300

    add org

--- models/UserUpdateHistory.js
+++ models/UserUpdateHistory.js
@@ -7,6 +7,12 @@ const userUpdateHistorySchema = new mongoose.Schema(
       ref: "User",
       required: true,
     },
+    organization: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+      index: true,
+    },
     updatedBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
