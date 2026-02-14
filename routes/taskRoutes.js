3b64539ded5e1b7449f96b6df7b249057b988f33
Tracey Kadenyi <treykadenyi@gmail.com>
2026-02-14 19:00:45 +0300

    upgrade task routes with authorization

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -9,20 +9,32 @@ const {
   deleteTask,
 } = require("../controllers/taskController");
 const authController = require("../controllers/authController");
+const {
+  authorize,
+  PERMISSIONS,
+  RESOURCES,
+} = require("../middleware/authmiddleware");
+
+// 🔐 Protect all task routes
+router.use(authController.protect);
 
 // Get all tasks
-router.get("/", authController.protect, getAllTasks);
+router.get("/", authorize(PERMISSIONS.VIEW, RESOURCES.TASK), getAllTasks);
 
 // Create a new task
-router.post("/", authController.protect, createTask);
-
-// Update a task by ID
-router.put("/:id", authController.protect, updateTask);
+router.post("/", authorize(PERMISSIONS.CREATE, RESOURCES.TASK), createTask);
 
 // Get task by id
-router.get("/:id", authController.protect, getTaskById);
+router.get("/:id", authorize(PERMISSIONS.VIEW, RESOURCES.TASK), getTaskById);
+
+// Update a task by ID
+router.put("/:id", authorize(PERMISSIONS.EDIT, RESOURCES.TASK), updateTask);
 
 // Delete a task by ID
-router.delete("/:id", authController.protect, deleteTask);
+router.delete(
+  "/:id",
+  authorize(PERMISSIONS.DELETE, RESOURCES.TASK),
+  deleteTask,
+);
 
 module.exports = router;
