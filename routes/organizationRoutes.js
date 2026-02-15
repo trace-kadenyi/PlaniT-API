92fadef3e84ebec8d9fde2433fbd5d3bf0f545de
Tracey Kadenyi <treykadenyi@gmail.com>
2026-02-15 16:20:27 +0300

    add authorization

--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -1,7 +1,9 @@
-// organizationRoutes.js
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../controllers/authController");
+const { authorize } = require("../middleware/authMiddleware");
+const { PERMISSIONS, RESOURCES } = require("../services/permissionService");
+
 const {
   addUserToOrganization,
   getOrganizationUsers,
@@ -10,21 +12,42 @@ const {
   getOrganizationDetails,
 } = require("../controllers/organizationController");
 
-router.use(protect); // All routes protected
+// All routes protected
+router.use(protect);
 
 // create new user
-router.post("/users", addUserToOrganization);
+router.post(
+  "/users",
+  authorize(PERMISSIONS.MANAGE_USERS, RESOURCES.USER),
+  addUserToOrganization,
+);
 
 // get all users within an org
-router.get("/users", getOrganizationUsers);
+router.get(
+  "/users",
+  authorize(PERMISSIONS.VIEW, RESOURCES.USER),
+  getOrganizationUsers,
+);
 
 // delete/remove a user from org
-router.delete("/users/:userId", removeUserFromOrganization);
+router.delete(
+  "/users/:userId",
+  authorize(PERMISSIONS.MANAGE_USERS, RESOURCES.USER),
+  removeUserFromOrganization,
+);
 
 // update the role of a user within org
-router.patch("/users/:userId/role", updateUserRole);
+router.patch(
+  "/users/:userId/role",
+  authorize(PERMISSIONS.MANAGE_USERS, RESOURCES.USER),
+  updateUserRole,
+);
 
 // get org details
-router.get("/", getOrganizationDetails);
+router.get(
+  "/",
+  authorize(PERMISSIONS.VIEW, RESOURCES.USER),
+  getOrganizationDetails,
+);
 
 module.exports = router;
