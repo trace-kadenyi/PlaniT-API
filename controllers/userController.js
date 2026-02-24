94ac0231f80fac91ea0e45c4581d89be4b7a926f
Tracey Kadenyi <treykadenyi@gmail.com>
2026-02-24 14:21:03 +0300

    remove unused code

--- controllers/userController.js
+++ controllers/userController.js
@@ -137,23 +137,6 @@ const updateUser = async (req, res) => {
 
     const isSelf = req.user._id.toString() === req.params.userId;
 
-    // If editing someone else, check permissions
-    // if (!isSelf) {
-    //   // Admins can't edit super admins
-    //   if (req.user.role === "admin" && targetUser.role === "super_admin") {
-    //     return res.status(403).json({
-    //       message: "Cannot edit super admins",
-    //     });
-    //   }
-
-    //   // Check if user has permission to edit others
-    //   if (!["super_admin", "admin"].includes(req.user.role)) {
-    //     return res.status(403).json({
-    //       message: "Only admins can edit other users",
-    //     });
-    //   }
-    // }
-
     // Track changes BEFORE modifying
     const changes = [];
     let updateType = "profile_update";
