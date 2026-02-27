d5dd3af83fef237c2686a71013d636d0df5168d4
Tracey Kadenyi <treykadenyi@gmail.com>
2026-02-27 16:44:27 +0300

    add PATCH route for orgs

--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const {
   getOrganizationDetails,
+  updateOrganization,
 } = require("../controllers/organizationController");
 const authController = require("../controllers/authController");
 const { authorize } = require("../middleware/authmiddleware");
@@ -16,6 +17,13 @@ router.get(
   "/",
   authorize(PERMISSIONS.VIEW, RESOURCES.ORGANIZATION),
   getOrganizationDetails,
+);
+
+// PATCH route - Super Admin only
+router.patch(
+  "/",
+  authorize(PERMISSIONS.EDIT, RESOURCES.ORGANIZATION),
+  updateOrganization,
 );
 
 module.exports = router;
